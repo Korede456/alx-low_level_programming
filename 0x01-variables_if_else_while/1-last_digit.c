fdd01bb4d3ce45af0be2afc667c33d0e513f684d
korede odeyemi <koredeodeyemi456@gmail.com>
2023-06-16 08:02:56 -0700

    A C program that prints the last number of a randomly generated interger

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - it prints the last number of a randomly
+ *	generated integer
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 3;
+	int l = abs(n % 10);
+
+	if (l > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l);
+	}
+	else if (l == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, l);
+	}
+
+	return (0);
+}
