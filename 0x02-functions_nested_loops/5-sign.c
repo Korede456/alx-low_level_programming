a06b5a3178dd58f4e5c6e6720e8212b6000e55e6
korede thompson <koredeodeyemi456@gmail.com>
2023-06-20 21:20:33 +0000

    A program that checks if an input is positive or negative

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - it checks if a number is positive or negative
+ * @n: is the character to be checked
+ * Return: 0, + or -
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	_putchar('-');
+	return ('-1');
+}
