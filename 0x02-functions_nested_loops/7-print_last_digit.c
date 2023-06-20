f25442749aecad7ed3b53ce1da416861181bac2d
korede thompson <koredeodeyemi456@gmail.com>
2023-06-20 21:46:26 +0000

    A program that prints the last digit of an interger

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_last_digit - it prints the last digit of an integer
+ * @c: is the parameter to be checked
+ * Return: 0 (success)
+ */
+
+int print_last_digit(int c)
+{
+	int last_digit = c % 10;
+
+	_putchar('0' + last_digit);
+
+	return (0);
+}
