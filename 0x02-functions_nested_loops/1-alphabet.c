40e13923669117a90d26310ec550f87f0147c8e4
korede thompson <koredeodeyemi456@gmail.com>
2023-06-20 11:02:33 +0000

    A function that prints alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - check the code
+ * Return: always (0)
+ */
+
+int main(void)
+{
+	int alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+This directory contains all my projects on functions and nested loops
