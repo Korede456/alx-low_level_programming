089adc85b5f4019f09d7e185bf2944784c335b3e
korede thompson <koredeodeyemi456@gmail.com>
2023-06-20 14:27:18 +0000

    A program that checks if a char is lowercase or uppercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - check if a number is uppercase or lowercase
+ * Return: 1 or 0
+ */
+
+int _islower(int c)
+{
+	if (_islower(c))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
