b3c3296575f3e498a0839fb38ede2e675bc79bdc
korede thompson <koredeodeyemi456@gmail.com>
2023-06-21 00:08:32 +0000

    A program that prints the sum of multiples

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,14 +8,11 @@
 
 int main(void)
 {
-	int i;
 	int sum;
 
 	for (i = 0; i < 1024; i++)
 	{
-		sum = 0;
-
-		if (i % 3 == 0 || i % 5 == 0)
+		if ((int i % 3) == 0 || (i % 5) == 0)
 		{
 			sum += i;
 		}
