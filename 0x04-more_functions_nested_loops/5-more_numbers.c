38b9adbc67a39ee7e9e8ec84a2f4356cb38f5aae
korede thompson <koredeodeyemi456@gmail.com>
2023-06-22 16:39:35 +0000

    fixed

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,13 +7,13 @@
 void more_numbers(void)
 {
 	int i;
-	int j;
+	int num;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (j = 0; j <= 14; j++)
+		for (num = 0; num <= 14; num++)
 		{
-			_putchar(j + '0');
+			_putchar(num + '0');
 		}
 		_putchar('\n');
 	}
