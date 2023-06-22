5543e2b863c06d00bfa48e67c85f38303ec3369c
korede thompson <koredeodeyemi456@gmail.com>
2023-06-22 16:06:29 +0000

    A function that prints a straight line

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -7,14 +7,15 @@
 
 void print_line(int n)
 {
-	if (n == 0)
+	int i;
+
+	if (n != 0)
 	{
-		_putchar('\n');
-		break;
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
 	}
 
-	for (i = 1; i <= n; i++)
-	{
-		_putchar('_');
-	}
+	_putchar('\n');
 }
