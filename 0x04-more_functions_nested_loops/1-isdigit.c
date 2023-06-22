50d298de33c7b197a31da1fc6ddae1e4c004e5cc
korede thompson <koredeodeyemi456@gmail.com>
2023-06-22 11:38:03 +0000

    a function that checks if an input is a digit'

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,12 @@
 /**
  * _isdigit - it checks if an input is a digit or not
  * @c: is the data to be checked
- * Return: 0 or 1
+ * Return: 1 or 1
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 || c <= 9)
+	if (c >= 0 && c <= 9)
 	{
 		return (1);
 	}
