c4bb0949550452090fe5b78074beb1540fbf20b1
korede thompson <koredeodeyemi456@gmail.com>
2023-06-22 18:22:34 +0000

    A function that prints a triangle pattern

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_triangle - it prints a triangle pattern
+ * @size: is the size of the pattern
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j < size; j++)
+			{
+				if (j + 1 <= size)
+				{
+					_putchar(' ');
+				}
+				_putchar('#');
+			}
+		}
+	}
+}
