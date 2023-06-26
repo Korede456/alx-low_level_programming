8f53d2b720eaf5dfd26123a85b57d80822b5c31a
korede thompson <koredeodeyemi456@gmail.com>
2023-06-26 17:30:47 +0000

    a function that swaps the values of two integers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * swap_int - it swaps the value of two integers
+ * @a: it's the first parameter
+ * @b: it's the second parameter
+ */
+
+void swap_int(int *a, int *b)
+{
+	*a, *b = *b, *a;
+}
--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -4,9 +4,11 @@
 /**
  * _putchar - it prints a char type
  * @c: is the parameter
+ * Return: always 0
  */
 
 void _putchar(char c)
 {
-	putchar(c)
+	putchar(c);
+	return (0);
 }
