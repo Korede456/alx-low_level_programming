5e10a751e0e923e42aef61d62bb9b79be1b5d174
korede odeyemi <koredeodeyemi456@gmail.com>
2023-06-26 09:54:43 -0700

    created a main.h file

---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This directory contains my ALX projects on pointers, arays and strings
---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _putchar - it prints a char type
+ * @c: is the parameter
+ */
+
+void _putchar(char c)
+{
+	putchar(c)
+}
