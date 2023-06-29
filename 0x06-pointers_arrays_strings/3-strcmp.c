524e2fe8b8f4da0ba854c6681f6d3e3fb26233ad
korede thompson <koredeodeyemi456@gmail.com>
2023-06-29 12:30:06 +0000

    a function that compares two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,9 +9,31 @@
 
 int _strcmp(char *s1, char *s2)
 {
+	int i;
+	int result;
+
 	if (s1 == s2)
 	{
 		return (0);
 	}
-	return (s1 - s2);
+	else
+	{
+		i = 0;
+
+		while (s1[i] != '\0' && s2[i] != '\0')
+		{
+			result = (s1[i] - s2[i]);
+
+			if (result != 0)
+			{
+				break;
+			}
+			else
+			{
+				continue;
+			}
+			i++;
+		}
+	}
+	return (result);
 }
