d3e2789c9b09624833274b62f83151df60c3db2d
korede thompson <koredeodeyemi456@gmail.com>
2023-07-04 22:30:27 +0000

    A function that pirnts the sum of the two diagonals of a quare matrix of integers

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - entry point
+ * @a: input
+ * @size: input
+ */
+
+void print_diagsums(int *a, int size)
+{
+	int sum1, sum2, y;
+
+	for (y = 0; y < size; y++)
+	{
+		sum1 = sum1 + a[y * size + y];
+	}
+
+	for (y = size - 1; y >= 0; y--)
+	{
+		sum2 += a[y * size + (size - y - 1)];
+	}
+
+	printf("%d, %d\n", sum1, sum2);
+}
