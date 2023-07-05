3dc6e40cf9e7ebb7434fbf416e95fc22262391fd
korede thompson <koredeodeyemi456@gmail.com>
2023-07-05 12:41:37 +0000

    {

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - entry point
+ * @s: input
+ */
+
+void _puts_recursion(char *s)
+{
+	char a = *s;
+
+	_putchar(a);
+	s++;
+
+	if (*s >= 0)
+	{
+		_puts_recursion(s);
+	}
+}
---
+++ 0x08-recursion/main.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+	_puts_recursion("puts with recursion");
+	return (0);
+}
