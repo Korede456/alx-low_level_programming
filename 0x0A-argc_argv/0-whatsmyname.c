438814a56627ad711e3f720a8bea2531f93c0170
korede thompson <koredeodeyemi456@gmail.com>
2023-07-07 20:52:12 +0000

    A program that prints the name of that program

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,6 +5,8 @@
  * main - entry point
  * @argc: number of arguments
  * @argv: array of arguments
+ *
+ * Return: always 0
  */
 
 void main(int argc __attribute__((unused)), char *argv[])
@@ -12,4 +14,6 @@ void main(int argc __attribute__((unused)), char *argv[])
 	char *programName = argv[0];
 
 	printf("%s\n", programName);
+
+	return (0);
 }
