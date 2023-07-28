b420c58d8be3fcfd311a0b368301297ad128920b
korede thompson <koredeodeyemi456@gmail.com>
2023-07-28 11:35:07 +0000

    a function that prints all elements of a string

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * print_list - it prints all the elements of a list
+ * @h: list node
+ * Return: it returns number of nodes
+ *
+ * Description: singly linked lists
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int num;
+
+	num = 0;
+
+	while (h)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)");
+		}
+		else
+		{
+			printf("[%u] %s", num, h->str);
+		}
+
+		num++;
+	}
+
+	return (num);
+}
