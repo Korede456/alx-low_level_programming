83c804fce66491edd00e8937d70060ca47b74369
korede thompson <koredeodeyemi456@gmail.com>
2023-08-02 17:11:15 +0000

    function that frees a listint_t list.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a list node and sets it to Null
+ * @head: the node to be freed
+ * Return: nothing
+ */
+
+void free_listint2(listint_t **head)
+{
+	free(*head);
+	*head = NULL;
+}
