7dd5415d9314588b0c107c6592ffd8883deb5c96
korede thompson <koredeodeyemi456@gmail.com>
2023-08-02 16:20:53 +0000

    Function that prints all elements of a linked list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -15,15 +15,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h)
 	{
-		if (!h->n)
-		{
-			return ('\0');
-		}
-		else
-		{
-			printf("%d\n", h->n);
-		}
-
+		printf("%d\n", h->n);
 		count++;
 		h = h->next;
 	}
