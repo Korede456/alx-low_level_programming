9694f68e88e29f3ff7bbbee8a17384364816b1ff
korede thompson <koredeodeyemi456@gmail.com>
2023-08-30 21:05:04 +0000

    it returns the square root of a given int

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -8,4 +8,49 @@
 
 int _sqrt_recursion(int n)
 {
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (root-recursion(n, 0, n / 2));
+	}
+}
 
+/**
+ * root-recursion - searches for the root
+ * @a: input
+ * @begin: input 2
+ * @end: input 3
+ * Return: integer always
+ */
+
+int root-recursion(int a, int begin, int end)
+{
+	int middle, squ;
+
+	if (begin <= end)
+	{
+		middle = begin + (end - begin) / 2;
+		squ = middle * middle;
+
+		if (squ == n)
+		{
+			return (middle);
+		}
+		else if (squ < n)
+		{
+			return (root-recursion(a, middle + 1, end));
+		}
+		else
+		{
+			return (root-recursion(a, begin, middle - 1));
+		}
+	}
+	return (-1);
+}
