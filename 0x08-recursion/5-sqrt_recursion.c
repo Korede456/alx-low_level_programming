9180578c89c350428ef518b78c711299a7505342
Korede Thompson <koredeodeyemi456@gmail.com>
2023-08-30 22:10:42 +0100

    A function that returns the square root of an int

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,39 @@
 #include "main.h"
 
 /**
+ * root-recursion - searches for the root
+ * @a: input
+ * @begin: input 2
+ * @end: input 3
+ * Return: integer always
+ */
+
+int _root_recursion(int a, int begin, int end)
+{
+	int middle, squ;
+
+	if (begin <= end)
+	{
+		middle = begin + (end - begin) / 2;
+		squ = middle * middle;
+
+		if (squ == n)
+		{
+			return (middle);
+		}
+		else if (squ < n)
+		{
+			return (_root_recursion(a, middle + 1, end));
+		}
+		else
+		{
+			return (_root_recursion(a, begin, middle - 1));
+		}
+	}
+	return (-1);
+}
+
+/**
  * _sqrt_recursion - entry point
  * @n: input integer
  * Return: returns integer always
@@ -18,39 +51,6 @@ int _sqrt_recursion(int n)
 	}
 	else
 	{
-		return (root-recursion(n, 0, n / 2));
+		return (_root_recursion(n, 0, n / 2));
 	}
-}
-
-/**
- * root-recursion - searches for the root
- * @a: input
- * @begin: input 2
- * @end: input 3
- * Return: integer always
- */
-
-int root-recursion(int a, int begin, int end)
-{
-	int middle, squ;
-
-	if (begin <= end)
-	{
-		middle = begin + (end - begin) / 2;
-		squ = middle * middle;
-
-		if (squ == n)
-		{
-			return (middle);
-		}
-		else if (squ < n)
-		{
-			return (root-recursion(a, middle + 1, end));
-		}
-		else
-		{
-			return (root-recursion(a, begin, middle - 1));
-		}
-	}
-	return (-1);
 }
