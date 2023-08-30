dfccd70a8e39cf8497494bd381374549be544db3
korede thompson <koredeodeyemi456@gmail.com>
2023-08-30 13:40:53 +0000

    A function that prints a string of characters

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ * _puts_recursion - entry point
+ * @s: input string
+ */
+
+void _puts_recursion(char *s)
+{
+	char later = *s;
+
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+	_putchar(a);
+	_puts-recursion(s + 1);
+}
