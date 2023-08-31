12e7fbb974024a667e43ce27449db0cd04dbbaa4
korede thompson <koredeodeyemi456@gmail.com>
2023-08-31 15:35:36 +0000

    compares two strings

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * wildcp - entry point
+ * @s1: pointer to string
+ * @s2: pointer to second string
+ * Return: 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == '\0')
+	{
+		if (*s2 != '\0')
+		{
+			return (wildcmp(s1, s2 + 1));
+		}
+		return (*s2 == '\0');
+	}
+	if (*s2 == '*')
+	{
+		return (wildcmp(s1 + 1, s2 + 1));
+	}
+	return (0);
+}
