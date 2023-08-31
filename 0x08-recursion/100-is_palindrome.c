22a5ca89775f48a273f5fcb992a303e65a3a1960
Korede Thompson <koredeodeyemi456@gmail.com>
2023-08-31 16:16:49 +0100

    Update 100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,6 +2,7 @@
 #include "main.h"
 
 int check_palindrome(char *s, int i, int length);
+int _strlen_recursion(char *s);
 
 /**
  * is_palindrome - entry point
@@ -16,6 +17,21 @@ int is_palindrome(char *s)
 		return (1);
 	}
 	return (check_palindrome(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ * _strlen_recursion - entry point
+ * @s: input string
+ * Return: an interger
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+	{
+		return (0);
+	}
+	return (1 + _strlen_recursion(s + 1));
 }
 
 /**
