6546b8e2649bac2270d4a59a77ca6d092c0d18d7
Korede Thompson <koredeodeyemi456@gmail.com>
2023-08-31 16:41:22 +0100

    A function that compares two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * wildcp - entry point
+ * wildcmp - entry point
  * @s1: pointer to string
  * @s2: pointer to second string
  * Return: 0
@@ -11,7 +11,7 @@ int wildcmp(char *s1, char *s2)
 {
 	if (*s1 == '\0')
 	{
-		if (*s2 != '\0')
+		if (*s2 != '\0' && *s2 == '*')
 		{
 			return (wildcmp(s1, s2 + 1));
 		}
