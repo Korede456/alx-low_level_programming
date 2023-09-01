cdd48ea11012da2dfd3329eac995c4234d239907
Elizabeth Oladeji <helloaleeyah@gmail.com>
2023-09-01 21:49:35 +0100

    argc and argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	char *programName = argv[0];
+
+	printf("%s\n", programName);
+
+	return (0);
+}
