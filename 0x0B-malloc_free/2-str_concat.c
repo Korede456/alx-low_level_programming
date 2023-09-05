b6de20e5da45cbbf5c09eb5fc9d717efd35fa638
korede thompson <koredeodeyemi456@gmail.com>
2023-09-05 12:31:06 +0000

    A program that concatenate two strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -15,20 +15,19 @@ char *str_concat(char *s1, char *s2)
 
 	if (s1 == NULL)
 	{
-		s2 = "";
+		s1 = "";
 	}
 	if (s2 == NULL)
 	{
-		s1 = "";
+		s2 = "";
 	}
-
-	strcpy(string, s1);
-	strcat(string, s2);
 
 	if (!string)
 	{
 		return (NULL);
 	}
+	strcpy(string, s1);
+	strcat(string, s2);
 
 	return (string);
 }
