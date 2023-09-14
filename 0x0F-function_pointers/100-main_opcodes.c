42bb7aa74afb6932b27218ddb13b9221ef9ebbf3
korede thompson <koredeodeyemi456@gmail.com>
2023-09-14 21:47:42 +0000

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -34,10 +34,10 @@ int main(int argc, char *argv[])
 	{
 		if (i == byte - 1)
 		{
-			printf("%02hhx\n", arr[i]);
+			printf("%02hhx\n", array[i]);
 			break;
 		}
-		printf("%02hhx ", arr[i]);
+		printf("%02hhx ", array[i]);
 	}
 	return (0);
 }
