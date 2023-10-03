8461e21e616e965d6542a2257009407fe6998aa8
Elizabeth Oladeji <helloaleeyah@gmail.com>
2023-10-03 13:47:07 +0100

    malloc and free

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -11,18 +11,19 @@ char *_strdup(char *str)
 {
 	char *str2;
 
-	str2 = (char *)malloc(strlen(str) + 1);
-
-	if (str == NULL)
+	if(str == NULL)
 	{
 		return (NULL);
 	}
-
-	if (str2 == NULL)
+	else
 	{
-		return (NULL);
-	}
+		str2 = (char *)malloc(strlen(str) + 1);
+		if (str2 == NULL)
+		{
+			return (NULL);
+		}
 
 	str2 = strcpy(str2, str);
+	}
 	return (str2);
 }
