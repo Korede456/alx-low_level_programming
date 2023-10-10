fe773545ec00ff7cb027488d27d6e321d7c4eb37
Elizabeth Oladeji <helloaleeyah@gmail.com>
2023-10-10 13:07:58 +0100

    c-preproccessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * main - Entry point of the program
+ *
+ * Description: This is the main function of the program
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
