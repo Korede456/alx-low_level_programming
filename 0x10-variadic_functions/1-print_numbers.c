61ede1143a25de43b6d74bd582df765d6f312d9b
korede thompson <koredeodeyemi456@gmail.com>
2023-10-12 21:23:51 +0000

    A variadic function that prints numbers

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,6 +15,11 @@ void print_numbers(const char *separator, const unsigned int n,
 	int arg;
 	va_list args;
 
+	if (n == 0)
+	{
+		return (NULL);
+	}
+
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
