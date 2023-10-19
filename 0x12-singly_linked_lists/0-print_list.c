60535ee4c6832e9fbef2a9e111a98b5db08067cd
korede thompson <koredeodeyemi456@gmail.com>
2023-10-19 12:18:39 +0000

    prints all elements of a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -10,17 +10,17 @@ size_t print_list(const list_t *h)
 {
 	size_t n;
 
-	n = 0;
-
-	if (h->str == NULL)
-	{
-		printf("[0] (nil)\n");
-	}
-	else
 	{
 		while (h != NULL)
 		{
-			printf("[%d] %s\n", h->len, h->str);
+			if (h->str == NULL)
+			{
+				printf("[0] (nil)\n");
+			}
+			else
+			{
+				printf("[%d] %s\n", h->len, h->str);
+			}
 			n++;
 			h = h->next;
 		}
