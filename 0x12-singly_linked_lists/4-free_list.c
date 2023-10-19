2b580fe5259ae5ed7a3b1ac253064ef99bb6d94f
korede thompson <koredeodeyemi456@gmail.com>
2023-10-19 14:51:48 +0000

    Function that frees a linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * free_list - free the list memory
+ * @head: first node
+ * Return: always 0;
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+	list_t *next_node;
+
+	current = head;
+
+	while (current->next != NULL)
+	{
+		next_node = current->next;
+		free(current->str);
+		free(current);
+		current = next_node;
+	}
+}
