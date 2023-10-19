06a09100ca26320f7ce7e59c5bbae7d98d36bad6
korede thompson <koredeodeyemi456@gmail.com>
2023-10-19 13:44:17 +0000

    It add a new element to the start of a list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,18 +10,18 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	struct list_s *new;
+	list_t *new;
 
-	new = (struct list_s *)malloc(sizeof(list_t));
+	new = (list_t *)malloc(sizeof(list_t));
 
 	if (new == NULL)
 	{
 		return (NULL);
 	}
 
-	new->str = strcpy(new->str, str);
+	new->str = strdup(str);
 	new->len = strlen(str);
-	new->next= (*head);
+	new->next = (*head);
 	(*head) = new;
 
 	return (*head);
