1d15157fc7e2facdbaa7b2f545a0ec633ef59eb6
korede thompson <koredeodeyemi456@gmail.com>
2023-10-23 21:22:49 +0000

    Returns nth node of a linked list

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -9,18 +9,14 @@
 
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-	unsigned int i, len;
-
-	i = 0;
-	len = listint_len(head);
-
-	if (head == NULL || index > len)
-	{
-		return (NULL);
-	}
+	unsigned int i = 0;
 
 	while (i <= index)
 	{
+		if (head == NULL)
+		{
+			return (NULL);
+		}
 		head = head->next;
 		i++;
 	}
