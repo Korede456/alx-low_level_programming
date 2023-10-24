741a6aaac263e7b4d4839cf148deb79b60abcadd
korede thompson <koredeodeyemi456@gmail.com>
2023-10-24 14:04:48 +0000

    prints the address an data of a linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -10,6 +10,11 @@ size_t print_listint_safe(const listint_t *head)
 {
 	size_t n = 0;
 
+	if (head == NULL)
+	{
+		exit(98);
+	}
+
 	while (head != NULL)
 	{
 		printf("[%p] %u\n", (void *)head, head->n);
