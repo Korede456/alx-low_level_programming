b9a7042a9359696de2e2707a270fb40b2ec39ed1
korede thompson <koredeodeyemi456@gmail.com>
2023-10-26 21:03:49 +0000

    Returns git at index

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * get_bit - returns the value of a it at
+ * a given index
+ *
+ * @n: number to be converted
+ * @index: index to be returned
+ * Return: bit at index
+ */
+
+int get_bit(unsigned int n, unsigned int index)
+{
+	unsigned int temp;
+
+	if (index >= sizeof(unsigned long int) * 8)
+	{
+		return (-1);
+	}
+
+	temp = 1UL << index;
+
+	if ((n & temp) == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (1);
+	}
+}
