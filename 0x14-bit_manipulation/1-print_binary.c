a070b323125f7698df371f2962c1806b00fd7d71
korede thompson <koredeodeyemi456@gmail.com>
2023-10-26 20:03:57 +0000

    Prints the binary representation of a given number

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_binary - prints binary
+ * @n: number to be printed
+ * Return: nothing
+ */
+
+void print_binary(unsigned long int n)
+{
+	int i;
+	unsigned long int temp;
+
+	temp = 1UL << (8 * sizeof(unsigned long int) - 1);
+
+	for (i = 0; i <8 * sizeof(unsigned long int); i++)
+	{
+		if (n & temp)
+		{
+			_putchar('1');
+		}
+		else
+		{
+			_putchar('0');
+		}
+		temp >>= 1;
+	}
+	_putchar('\n');
+}
