835576904bb5c41345d47a105e8a0b4b122ae851
korede thompson <koredeodeyemi456@gmail.com>
2023-10-26 13:50:48 +0000

    Converts binary to unsigned int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -22,7 +22,7 @@ unsigned int binary_to_uint(const char *b)
 	{
 		if (b[i] == '0' || b[i] == '1')
 		{
-			result = (result << 1) | ([i] - '0');
+			result = (result << 1) | (b[i] - '0');
 			i++;
 		}
 		else
