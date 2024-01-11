9145fbe73a45b1a0a5d04755cda54af4f5e7747a
korede thompson <koredeodeyemi456@gmail.com>
2024-01-11 22:03:28 +0000

    List Length: returns the length of a doubly linked list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * slistint_len - calculate the length of a list
+ * @h: points to the head node
+ *
+ * Return: the number of nodes
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count;
+	dlistint_t *current;
+
+	count = 0;
+	current = malloc(sizeof(dlistint_t));
+	current->n = h->n;
+	current->next = h->next;
+	current->prev = h->prev;
+
+	while (current)
+	{
+		count += 1;
+		current = current->next;
+	}
+	free(current);
+	return (count);
+}
