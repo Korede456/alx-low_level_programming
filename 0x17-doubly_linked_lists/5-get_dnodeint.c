fed8e289eb4cda0930413c4423783f353872f541
korede thompson <koredeodeyemi456@gmail.com>
2024-01-12 09:40:13 +0000

    Return node:  returns the nth node of a doubly linked list

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -19,7 +19,7 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (current != NULL && count != index)
 	{
 		current = current->next;
-		count ++;
+		count++;
 	}
 	return (current);
 }
