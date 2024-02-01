fa52fc9f72892d49d6a93663da897ccf582c228e
korede thompson <koredeodeyemi456@gmail.com>
2024-02-01 20:43:06 +0000

    hash table

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,21 @@
+#include "hash_tables.c"
+
+/**
+ * hash_djb2 - generates a hash code
+ * @str: string pointer
+ *
+ * Return: a long int;
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	while ((c = *str++))
+	{
+	hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	}
+	return (hash);
+}
