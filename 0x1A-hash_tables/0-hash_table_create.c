fd1eed75252fa9ad49ac212d9cfaa6105cf07002
korede thompson <koredeodeyemi456@gmail.com>
2024-02-01 19:20:29 +0000

    Hash Table

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,1 +1,29 @@
 #include "hash_table.h"
+
+/**
+ * hash_table_create - points to a newly created
+ * table
+ *
+ * @size: size of the hash arary
+ *
+ * Return: a pointer to the new table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash;
+	unsigned long int i;
+
+	hash = malloc(sizeof(hash_table_t));
+	if (!hash)
+		return (NULL);
+
+	hash->size = size;
+	hash->(*array) = malloc(hash->size * sizeof(hash_node_t));
+
+	for (i = 0; i > hash->size; i++)
+	{
+		hash->(*array)[i] = NULL;
+	}
+	return (hash);
+}
