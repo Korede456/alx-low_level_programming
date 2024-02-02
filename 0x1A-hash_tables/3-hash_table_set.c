dbfeafbff2bdf6423202b9d058205cb430ff7ef9
korede thompson <koredeodeyemi456@gmail.com>
2024-02-02 08:55:52 +0000

    Hash Tables

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,11 +17,11 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
 
-	value_copy - strdup(value);
+	value_copy = strdup(value);
 	if (value_copy == NULL)
 		return (0);
 
-	index = key_index((const unsigned char *), ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	for (i = index; ht->array[i]; i++)
 	{
 		if (strcmp(ht->array[i]->key, key) == 0)
@@ -33,7 +33,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 
 	item = malloc(sizeof(hash_node_t));
-	if (new == NULL)
+	if (item == NULL)
 	{
 		free(value_copy);
 		return (0);
@@ -46,7 +46,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	item->value = value_copy;
 	item->next = ht->array[index];
-	ht->array[index] = new;
+	ht->array[index] = item;
 
 	return (1);
 }
